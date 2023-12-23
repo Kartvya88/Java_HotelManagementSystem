715cfe4ef25f44371d14e8fc9270392381165560
Kartvya88 <96675668+Kartvya88@users.noreply.github.com>
2023-12-23 06:25:11 -0500

    Login Functionality Added

--- src/HotelManagement.java
+++ src/HotelManagement.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class HotelManagement extends JFrame {
+//Implemented Action Listener Interface
+public class HotelManagement extends JFrame implements ActionListener {
 
     HotelManagement(){
  setSize(1366,565 );
@@ -8,12 +12,46 @@ public class HotelManagement extends JFrame {
  ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
  JLabel image = new JLabel(i1);
  add(image);
- setVisible(true);
- 
+
+ //Adding a Hotel Management Systm Text above the image
+        JLabel text = new JLabel("Hotel Management System");
+        text.setBounds(25,430,1000,90);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("serif", Font.PLAIN, 50));
+        image.add(text);
+
+  //Adding a NEXT button above Image
+  JButton btn = new JButton("Next");
+  btn.setFont(new Font("Arial", Font.ITALIC, 20));
+  btn.setBounds(930,450, 150,50);
+  image.add(btn);
+
+setVisible(true);
+
+  //Blink functionality in the TEXT
+        while(true){
+            text.setVisible(false);
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            text.setVisible(true);
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
     }
     public static void main(String[] args) {
         new HotelManagement();
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
---
+++ src/Login.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.stream.Collectors;
+
+public class Login extends JFrame {
+    //Constructor
+    Login(){
+       //setting the frame
+ getContentPane().setBackground(Color.WHITE);
+ setLayout(null);
+
+ JLabel userName = new JLabel("User Name");
+ userName.setBounds(40,20,100,30);
+ add(userName);
+
+ JLabel password = new JLabel("Password");
+ password.setBounds(40,70,100,30);
+ add(password);
+
+ JTextField userField = new JTextField();
+ userField.setBounds(150, 20, 150, 30);
+ add(userField);
+
+
+ JTextField passField = new JTextField();
+ passField.setBounds(150, 70, 150, 30);
+ add(passField);
+
+ //Login Button Design
+ JButton login = new JButton("Login");
+ login.setBackground(Color.BLACK);
+ login.setForeground(Color.WHITE);
+ login.setFont(new Font("Serif", Font.PLAIN, 15));
+ login.setBounds(40,150,120,30);
+ add(login);
+
+ //Cancel Button Design
+        JButton cancel = new JButton("Cancel");
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        cancel.setFont(new Font("Serif", Font.PLAIN, 15));
+        cancel.setBounds(40,200,120,30);
+        add(cancel);
+
+        //Adding a Image in the Layout
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel imgLabel = new JLabel(i3);
+        imgLabel.setBounds(350,10,200,200);
+        add(imgLabel);
+
+ setBounds(500,200,600,300);
+ setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+        //Login object calling the constructor
+    }
+}
